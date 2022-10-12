6b7ee8894cf109eea413d1496445fea03218ebf7
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-12 09:59:06 +0100

    first commit

---
+++ src/test/java/com/iset/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iset.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
